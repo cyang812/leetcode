d8d0ecf99656673330183cd2af3dfe1145d9c7f4
cyang <cy950812@gmail.com>
2017-07-20 18:07:39 +0800

    modify countAndSay.c

--- countAndSay.c
+++ countAndSay.c
@@ -8,50 +8,65 @@
 #include <string.h>
 #include <stdlib.h>
 
+char *mystrcat(char *dst,const char *src) //用自己的方式实现strcat函数功能
+{
+    char *p=dst;  //下面的操作会改变目的指针指向，先定义一个指针记录dst
+    while(*p!='\0')p++;
+    while(*src != '\0')*p++=*src++;
+    *p='\0';
+
+    return p;  //dst现在指向拼接后的最后一位字符，在这里返回dst，会出现错误
+}
+
+
 char* countAndSay(int n) {
 
     //char *s = (char*)malloc((n)*sizeof(char));
-    char *s = (char*)malloc((10)*sizeof(char));
-    char temp[3];
-    int i,idx;
+    char s[200] = " ",*strPtr = s;
+    char temp[30];
+    int i,idx = 1;
     int count = 0;
 
     if(n == 1)
     {
-        s = "11";
-        //printf("n==1\n");
+        strPtr = "11";
+        printf("n==1\n");
         //printf("s = %d\n",strlen(s));
-        return s;
+        return strPtr;
     }
     else
     {
-        s = countAndSay(n-1);
-        printf("str = %s , strlen = %d\n",s,strlen(s));
-        for(i=0,count=0; count<strlen(s);i++ )
+        strPtr = countAndSay(n-1);
+        printf("str = %s , strlen = %d\n",strPtr,strlen(strPtr));
+        for(i=0; count<strlen(strPtr);i++ )
         {
-            idx=1;
-            if(s[i] == s[i+1])
+            printf("loop\n");
+            if(strPtr[i] == strPtr[i+1])
             {
+                printf("idx++\n");
                 idx++;
             }else
             {
-                sprintf(temp,"%d%d",idx,s[i]);
-                printf("idx = %d, s[i] = %d, temp = %s \n",idx,s[i],temp);
-                //strcat(s,temp);
+                sprintf(temp,"%d%c",idx,strPtr[i]);
+                printf("idx = %d, s[i] = %c, temp = %s \n",idx,strPtr[i],temp);
+
+                //toDo:strcat
+                strcat(s,temp);
                 count+=idx;
+                idx = 1;
             }
         }
     }
-
+    strPtr = s;
     printf("s = %s\n",s);
-    return s;
+    return strPtr;
 
 }
 
 
 int main(){
 
-    int a = 2;
+    int a = 3;
     char *str;
 
     str = countAndSay(a);
