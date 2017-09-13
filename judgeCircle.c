1f6c6c575b1322ad53cc7b0d3855e5b397c4600c
cyang <cy950812@gmail.com>
2017-09-13 14:27:45 +0800

    add c file

---
+++ judgeCircle.c
@@ -0,0 +1,54 @@
+/*
+* 657. Judge Route Circle
+* three ways to solution this problem
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool judgeCircle(char* moves) {
+
+   	int upCnt = 0;
+	int downCnt = 0;
+	int rightCnt = 0;
+	int leftCnt = 0;
+
+	while(*moves)
+	{
+		switch(*moves)
+		{
+			case 'U':
+				upCnt++;
+				break;
+			case 'D':
+				downCnt++;
+				break;
+			case 'R':
+				rightCnt++;
+				break;
+			case 'L':
+				leftCnt++;
+				break;
+		}
+		moves++;
+	}
+
+    printf("upCnt = %d , downCnt = %d \n",upCnt,downCnt);
+	printf("rightCnt = %d , leftCnt = %d \n",rightCnt,leftCnt);
+
+	if((upCnt == downCnt) && (rightCnt == leftCnt))
+	{
+		return true;
+	}
+	else
+		return false;
+}
+
+int main()
+{
+    char *test_str = "RRLLDDUU";
+    bool ret = false;
+
+    ret = judgeCircle(test_str);
+    printf("ret = %d\n",ret);
+}
