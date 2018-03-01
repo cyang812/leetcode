b4b06877d545ce46bb977c1678d34bdaeae4b4f9
cyang <cy950812@gmail.com>
2018-03-01 14:36:29 +0800

    add

---
+++ nextGreaterElement_1.c
@@ -0,0 +1,76 @@
+/*
+*496. Next Greater Element I
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COUNTOF(x)  (sizeof(x)/sizeof(x[0]))
+
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* nextGreaterElement(int* findNums, int findNumsSize, int* nums, int numsSize, int* returnSize) {
+    
+	int *ret = (int *)malloc(sizeof(int)*findNumsSize);
+	int flag = 0;
+
+	*returnSize = findNumsSize;
+
+	for (int i = 0; i < findNumsSize; ++i)
+	{
+		/* code */
+		ret[i] = -1;
+		flag = 0;
+
+		for (int j = 0; j < numsSize; ++j)
+		{
+			/* code */
+			if (findNums[i] == nums[j])
+			{
+				flag = 1;
+			}
+			else if (flag == 1 && findNums[i] < nums[j] )
+			{
+				/* code */
+				ret[i] = nums[j];
+				flag = 2;
+			}
+		}
+	}
+
+	return ret;
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	// int a1[] = {4,1,2};
+	// int b1[] = {1,3,4,2};
+
+	int a1[] = {2,4};
+	int b1[] = {1,2,3,4};
+
+	int *ret = NULL;
+	int returnSize;
+
+	ret = nextGreaterElement(a1,COUNTOF(a1),b1,COUNTOF(b1),&returnSize);
+
+	for (int i = 0; i < returnSize; ++i)
+	{
+		/* code */
+		printf("%d ", ret[i]);
+	}
+	printf("\n");
+
+	if (ret != NULL)
+	{
+		/* code */
+		printf("free\n");
+		free(ret);
+		ret = NULL;
+	}
+
+	return 0;
+}
---
+++ nextGreaterElement_3.c
@@ -0,0 +1,24 @@
+/*
+*556. Next Greater Element III
+*/
+
+#include <stdio.h>
+
+int nextGreaterElement(int n) {
+    
+    
+
+}
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	int a1 = 12;
+	int ret = 0;
+
+	ret = nextGreaterElement(a1);
+
+	printf("ret = %d\n", ret);
+
+	return 0;
+}
