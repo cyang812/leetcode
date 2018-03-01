ff6439420c351edabcec7c6b75b1e2617bea33c3
cyang <cy950812@gmail.com>
2018-03-01 09:26:44 +0800

    modify myAtoi.c

--- myAtoi.c
+++ myAtoi.c
@@ -9,8 +9,10 @@ int myAtoi(char* str) {
 	long long ret = 0;
 	int tmp = 0;
 	int flag = 0;
+	int err = 0;
+	int cnt = 0;
 
-	while(*str)
+	while(*str && err==0)
 	{
 		tmp = *str - '0';	
 
@@ -31,6 +33,8 @@ int myAtoi(char* str) {
 			else if(tmp <= 9 && tmp >= 0)
 			{
 				ret = ret*10 + tmp;
+				cnt++;
+				flag = 1;
 			}
 			else
 				break;
@@ -40,24 +44,31 @@ int myAtoi(char* str) {
 			if(tmp <= 9 && tmp >= 0)
 			{
 				ret = ret*10 + tmp;
+				cnt++;
 			}
 			else
-				break;
+			{
+				err = 1;
+			}	
 		}
 
 		str++;
 	}
 
+	printf("tmp = %d\n", ret);
+
 	if(flag == 2)
 		ret = -ret;
 
-	if(ret > (int)(0x7fffffff))
-		return 0x7fffffff;
+	if( (ret > (int)(0x7fffffff)) || (flag==1 && cnt>10) )
+		ret = 0x7fffffff;
 
-	if(ret < (int)(0x80000000))
-		return 0x80000000;
+	if( (ret < (int)(0x80000000)) || (flag==2 && cnt>10) )
+		ret = 0x80000000;
 
 	printf("flag = %d\n", flag);
+	printf("err = %d\n", err);
+	printf("cnt = %d\n", cnt);
 
 	return ret;
 }
@@ -67,7 +78,9 @@ int main(int argc, char const *argv[])
 {
 	/* code */
 	// char str[] = "2147483648";
-	char str[] = "123    456";
+	// char str[] = "123    456";
+	// char str[] = "9223372036854775809";
+	char str[] = "-9223372036854775809";
 
 	int ret = 0;
 
