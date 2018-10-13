463a20954ff6679b803fcb1a61745d514fb62fa3
cyang <cy950812@gmail.com>
2018-10-13 17:35:22 +0800

    abs

--- 754-Reach a Number.c
+++ 754-Reach a Number.c
@@ -2,8 +2,17 @@
 * @Author: cyang
 * @Date:   2018-10-13 15:55:01
 * @Last Modified by:   cyang
-* @Last Modified time: 2018-10-13 16:48:32
+* @Last Modified time: 2018-10-13 17:30:36
 */
+
+
+/*
+ *分析如下：首先一次相加直到和大于目标，比较差值，
+ *	       如果是偶数，说明只需要将几个数由加号变为减号（里外里2倍，所以一定是偶数），
+ *		   若是奇数，就要想办法制造1，减去一个奇数加上下一个偶数即可，
+ *		   所以如果当前n是个奇数，就需要加上下一个偶数，如果当前是个偶数就需要减去下一个奇数，加上下一个偶数。
+ *
+ */
 
 #include <stdio.h>
 #include <math.h>
@@ -50,21 +59,26 @@ int reachNumber(int target) {
 	}
 
 	int dt = temp - target;
+	int ret = 0;
 	if (dt % 2 == 0)
 	{
-		return cnt;
+		ret = cnt;
 	}
 	else
 	{
 		if (cnt % 2 == 0)
 		{
-			return cnt + 1;
+			ret = cnt + 1;
 		}
 		else
 		{
-			return cnt + 2;
+			ret = cnt + 2;
 		}
 	}
+	printf("target = %d, temp = %d, dt = %d, cnt = %d, ",target, temp, dt, cnt);
+	printf("need %d\n", ret);
+
+	return ret;
 #endif
 }
 
@@ -80,16 +94,7 @@ int main(int argc, char const *argv[])
 		int temp1, temp2;
 
 		temp1 = reachNumber(i);
-		temp2 = reachNumber(-i);
-
-		if (temp1 != temp2)
-		{
-			printf("idx = %d\n", i);
-		}
-		else
-		{
-			printf("idx %d need %d steps\n", i, temp1);
-		}
+		// printf("need %d\n", temp1);
 	}
 #endif
 	return 0;
